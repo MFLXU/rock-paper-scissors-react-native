0def2d4f1040bc87b2d10472218cf5382797b436
Lou+^*! <90705868+MFLXU@users.noreply.github.com>
2023-01-07 18:04:55 +0100

    project init

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.mainTitleContainer}>
+        <Text style={styles.mainTitle}>Rock, Papers, Scissors</Text>
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  mainTitleContainer: {
+    backgroundColor: "#000",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 20,
+  },
+  mainTitle: {
+    fontSize: 20,
+    color: "#fff",
   },
 });
